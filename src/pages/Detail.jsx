5b066101d0ec522987b71188b7ae28fd170f2369
Craig <indigomx9@gmail.com>
2022-10-05 00:52:53 -0400

    Updated Detail component and created Detail.css,.

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { toast } from "react-toastify";
+import { useParams } from "react-router-dom";
+import { skipToken } from "@reduxjs/toolkit/dist/query";
+import { BlogAPI } from "../global/BlogAPI";
 
 export const Detail = () => {
+    const { id } = useParams();
+    const { error, isError, data: blog } = 
+        BlogAPI.useGetOneQuery(id ? id : skipToken);
+
+    React.useEffect(() => {
+        isError && toast.error(error);
+    }, [isError]);
+
     return (
-        <React.Fragment>
-            <h1>Detail</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, dolore atque porro corporis quia aliquam recusandae voluptas ullam, odio mollitia maiores labore voluptate nostrum deserunt eaque tempore vel consectetur eum.</p>
-        </React.Fragment>
+        <main className="detail">
+            <img
+                className="detail__img" 
+                src={blog?.imgURL} 
+                alt={blog?.title} 
+            />
+            <section className="detail__body">
+                <title className="detail__title">
+                    {blog?.title}
+                </title>
+                <section className="detail__text">
+                    <span>Created at - &nbsp;</span>
+                    <small>
+                        {blog?.timestamp.toDate()
+                            .toLocaleString()}
+                    </small>
+                </section>
+                <section>
+                    <p>{blog?.description}</p>
+                </section>
+            </section>
+        </main>
     );
 };
 
